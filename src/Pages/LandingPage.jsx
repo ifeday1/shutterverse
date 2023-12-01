8feff48f0b3a7719740e941ad6d5442042e69b18
ifeoluwa oladeni <oladeniifeoluwa123@gmail.com>
2023-12-01 18:14:49 +0100

    Progress

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -8,11 +8,13 @@ const LandingPage = () => {
       <div class="relative">
         <div className="where">
           <h1>Where moments become timeless memories</h1>
-          <div class="absolute">
+          <div class="">
             <img
               style={{
                 width: "600px",
+                paddingTop: "80px",
                 top: "200px",
+
                 float: "right",
                 zIndex: "10",
               }}
@@ -24,20 +26,13 @@ const LandingPage = () => {
       </div>
       {/* About */}
       <div className="bg-dark-brown w-80 h-96">
-        <h2 className="mt-96 pl-16 pr-16">About</h2>
+        <h2 className="mt-96 pl-16 pr-16 ">About</h2>
         <h1 className="text-lg">
           I specialize in creating stunning visual stories through the lens.
           From weddings to family portraits, events to landscapes, every
           photograph I take encapsulates emotions, beauty, and unforgettable
           moments
         </h1>
-      </div>
-
-      <div className="bg-gray-200 p-4">
-        <h1 className="text-2xl font-bold text-center">Hello, Tailwind CSS!</h1>
-        <p className="text-gray-600 mt-2">
-          Tailwind CSS is set up in this React project.
-        </p>
       </div>
     </>
   );
